c55b0d016d1f789c8d7c5ae76affd614901d7c95
Michał Mikołajewski <m.mikolajewski02@gmail.com>
2024-01-09 10:25:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
   openWeatherMap: {
     BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-    SECRET_KEY: "3d0e75b1809f78f44935c36ada7b307f",
+    SECRET_KEY: "",
   },
 };
 
